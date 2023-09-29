54769ce6e030fa8e9209871f1597cc9ddb2ce25d
Arjun Goray <goraya25@lsrhs.net>
2023-09-29 11:18:17 -0400

    new server upload name, gitignore uploads/

--- website/src/server.js
+++ website/src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 3433;
@@ -12,7 +13,14 @@ app.use(cors());
 // Storage shit
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        const date = new Date();
+        const folderName = 'uploads/' + date.toISOString().slice(0, 10) + '_' + date.getHours() + ':' + date.getMinutes();
+
+        if (!fs.existsSync(folderName)) {
+            fs.mkdirSync(folderName, { recursive: true });
+        }
+
+        cb(null, folderName + '/');
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
