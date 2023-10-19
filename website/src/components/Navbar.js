369da76e36403d3e0cef454906d5df68d2ba939d
Sean Mabli <70709738+seanmabli@users.noreply.github.com>
2023-10-19 18:16:34 +0000

    differnt navbars depending on auth or not

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -7,19 +7,35 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 export default function EmailVerification() {
-  const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
   const navigate = useNavigate();
 
   async function handleLogout() {
-    setError("");
-
     try {
       await logout();
       navigate("/login");
     } catch {
-      setError("Failed to log out");
+      // Handle error
     }
+  }
+
+  if (!currentUser) {
+    return (
+      <div style={{ backgroundColor: "inherit" }}>
+        <Navbar bg="inherit" expand="lg">
+          <Navbar.Brand onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
+            FRC Dataset Colab
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Link onClick={() => navigate("/docs")} style={{ cursor: "pointer" }}>Docs</Nav.Link>
+              <Nav.Link onClick={() => navigate("/login")} style={{ cursor: "pointer" }}>Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
   }
 
   return (
@@ -31,7 +47,6 @@ export default function EmailVerification() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link onClick={() => navigate("/dashboard")} style={{ cursor: "pointer" }}>Dashboard</Nav.Link>
             <Nav.Link onClick={() => navigate("/upload")} style={{ cursor: "pointer" }}>Upload</Nav.Link>
             <Nav.Link onClick={() => navigate("/download")} style={{ cursor: "pointer" }}>Download Dataset</Nav.Link>
             <NavDropdown title="Account" id="basic-nav-dropdown">
