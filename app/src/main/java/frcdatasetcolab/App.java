86d2016201116ff4e6e036150d968a3973bab04f
seanmabli <seanmabli4@gmail.com>
2023-10-19 18:08:10 -0400

    work on folder vs individual file upload

--- app/src/main/java/frcdatasetcolab/App.java
+++ app/src/main/java/frcdatasetcolab/App.java
@@ -3,6 +3,7 @@ package frcdatasetcolab;
 import io.javalin.Javalin;
 import io.javalin.util.FileUtil;
 import io.javalin.http.UploadedFile;
+import io.javalin.http.staticfiles.Location;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import io.javalin.community.ssl.SSLPlugin;
@@ -14,47 +15,73 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class App {
     public static void main(String[] args) {
-	Javalin app = Javalin.create(config -> {
+        // Initialize Firebase outside of the route handler
+        try {
+            FileInputStream serviceAccount = new FileInputStream("/home/team4169/frcdatasetcolab/app/src/main/java/frcdatasetcolab/admin.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return; // Exit if there's an error with the service account file
+        }
+
+        Javalin app = Javalin.create(config -> {
             config.plugins.enableCors(cors -> {
                 cors.add(corsConfig -> {
                     corsConfig.anyHost();
                 });
             });
-	    config.plugins.register(new SSLPlugin(ssl->{
-	    ssl.host = "10.0.0.142";
-	    ssl.insecurePort=7070;
-	    ssl.securePort=3433;
-            ssl.pemFromPath("fullchain.pem", "privkey.pem");
-    }));
-        })
-            .get("/", ctx -> ctx.result("Hello World"))
-            .start();
-        
+            config.plugins.register(new SSLPlugin(ssl -> {
+                ssl.host = "10.0.0.142";
+                ssl.insecurePort = 7070;
+                ssl.securePort = 3433;
+                ssl.pemFromPath("fullchain.pem", "privkey.pem");
+            }));
+        }).start();
+
         app.post("/upload", ctx -> {
             try {
-                FileInputStream serviceAccount = new FileInputStream("/home/team4169/frcdatasetcolab/app/src/main/java/frcdatasetcolab/admin.json");
-                FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
-                FirebaseApp.initializeApp(options);
+                // The Firebase initialization is done outside the route handler
 
-                try {
-                    FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(ctx.header("idToken"));
-                    String uid = decodedToken.getUid();
-                    
-                    Date date = new Date();
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm");
-                    String formattedDate = dateFormat.format(date);
-                    ctx.uploadedFiles("files").forEach(uploadedFile -> FileUtil.streamToFile(uploadedFile.content(), "upload/" + formattedDate + '_' + uploadedFile.filename()));
-                } catch (FirebaseAuthException e) {
-                    e.printStackTrace();
+                FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(ctx.header("idToken"));
+                String uid = decodedToken.getUid();
+
+                Date date = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm");
+                String formattedDate = dateFormat.format(date);
+
+                // Save each uploaded file with original folder structure preserved
+                for (UploadedFile uploadedFile : ctx.uploadedFiles("files")) {
+                    String filePath = "upload/" + formattedDate + uploadedFile.filename();
+                    System.out.println(filePath);
+
+                    try (InputStream fileContent = uploadedFile.content();
+                         OutputStream output = new FileOutputStream(filePath)) {
+                        byte[] buffer = new byte[8192];
+                        int bytesRead;
+                        while ((bytesRead = fileContent.read(buffer)) != -1) {
+                            output.write(buffer, 0, bytesRead);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        // Sending an error response to the client
+                        ctx.status(500).result("Error: Failed to save the file on the server.");
+                        return; // Exit the loop and return an error response
+                    }
                 }
-            } catch (IOException e) {
+            } catch (FirebaseAuthException e) {
                 e.printStackTrace();
+                ctx.status(401).result("Error: Authentication failed.");
             }
         });
     }
-    
 }
--- website/src/pages/Upload.js
+++ website/src/pages/Upload.js
@@ -6,48 +6,95 @@ import axios from 'axios';
 export default function Dashboard() {
   const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
-
   let navigate = useNavigate();
 
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [uploadMode, setUploadMode] = useState("files"); // Default to individual files
+  const [isLoading, setLoading] = useState(false);
 
   const onFileChange = event => {
-    setSelectedFiles(event.target.files); // No need to spread the files, they are already an array
+    setSelectedFiles(event.target.files);
   };
 
   const onUpload = async () => {
+    if (isLoading) {
+      return;
+    }
+
+    setLoading(true);
+
     const formData = new FormData();
 
-    for (let i = 0; i < selectedFiles.length; i++) {
-      formData.append('files', selectedFiles[i]);
+    if (uploadMode === "folder") {
+      formData.append('folder', selectedFiles[0]);
+    } else {
+      for (let i = 0; i < selectedFiles.length; i++) {
+        formData.append('files', selectedFiles[i]);
+      }
     }
 
     try {
       const idToken = await currentUser.getIdToken();
       let config = {
         headers: {
-          idToken: idToken,
-          'Content-Type': 'multipart/form-data', // Add this header for FormData
+          'idToken': idToken, // Add the idToken as a header
+          'Content-Type': 'multipart/form-data',
         }
       };
 
       await axios.post('https://api.seanmabli.com:3433/upload', formData, config);
       alert('Files uploaded successfully');
     } catch (error) {
-      alert('Error uploading files');
+      if (error.response) {
+        // The request was made, but the server responded with an error status
+        if (error.response.status === 500) {
+          alert('Server error: Failed to save the file on the server.');
+        } else {
+          alert('Error: ' + error.message);
+        }
+      } else if (error.request) {
+        // The request was made, but no response was received (network error)
+        alert('Network error: Unable to communicate with the server.');
+      } else {
+        // Something happened in setting up the request (request configuration error)
+        alert('Request error: Unable to send the request.');
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div style={{ padding: "20px" }}>
-      <h2>Welcome, {currentUser.email}</h2>
       {error && <div className="alert alert-danger">{error}</div>}
       <div className="form-group" style={{ marginBottom: "20px" }}>
-        <input type="file" className="form-control-file" onChange={onFileChange} multiple />
+        <label>
+          Upload Mode:
+          <select
+            className="form-control"
+            onChange={e => setUploadMode(e.target.value)}
+            value={uploadMode}
+          >
+            <option value="files">Individual Files</option>
+            <option value="folder">Folder</option>
+          </select>
+        </label>
+      </div>
+      <div className="form-group" style={{ marginBottom: "20px" }}>
+        <label>
+          Select {uploadMode === "folder" ? "a Folder" : "Files"}:
+          <input
+            type="file"
+            className="form-control-file"
+            onChange={onFileChange}
+            multiple={uploadMode === "files"}
+            webkitdirectory={uploadMode === "folder" ? "webkitdirectory" : undefined}
+          />
+        </label>
       </div>
       <div className="text-center">
-        <button className="btn btn-primary" onClick={onUpload}>
-          Upload
+        <button className="btn btn-primary" onClick={onUpload} disabled={isLoading}>
+          {isLoading ? "Uploading..." : "Upload"}
         </button>
       </div>
     </div>
