359f2c950e949b650ccfa12669b07063b451b7dd
Sean Mabli <70709738+seanmabli@users.noreply.github.com>
2024-02-24 03:33:23 +0000

    basic rust lambda

---
+++ aws-lambda-server/model-performance/src/main.rs
@@ -0,0 +1,32 @@
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first("name"))
+        .unwrap_or("world");
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    println!("{}", message);
+
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .with_target(false)
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
